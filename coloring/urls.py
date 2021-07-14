3a4f69773604523edf449ad649efbba8342e0825
Moonwon Lee <moonwonlee@berkeley.edu>
2021-07-13 20:24:51 -0700

    working on homepage and connection between views

--- coloring/urls.py
+++ coloring/urls.py
@@ -4,5 +4,14 @@ from . import views
 urlpatterns = [
     path('demo', views.index, name='demo'),
     path('new_interaction', views.index, name='new_interaction'),
-    path('homepage', views.home, name='homepage')
+    path('homepage', views.home, name='homepage'),
+    path('mybook', views.mybook, name='mybook'),
+    path('templates', views.templates, name='templates'),
+    
+    # path('', views.index, name='index'),
+
+    # 
+    # path('3/', views.three, name='three'),
+    # path('4/', views.four, name='four'),
+    # path('5/', views.five, name='five'),
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def home(request):
     return render(request, 'coloring/homepage.html')
+
+def mybook(request):
+    return render(request, 'coloring/mybook.html')
+
+def templates(request):
+    return render(request, 'coloring/templates.html')
